59db496984230e9b7380a21b91f51abcb2b3dd86
eugene-black66 <zhenyatina123@gmail.com>
2022-07-21 22:42:18 +0300

    new feature: changecolor button

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
-class Comment {
-    constructor (text) {
-        this.text = text
-        this.likesQty = 0
-        this.dislikesQty = 0
-    }
-    like () {
-        this.likesQty +=1
-    }
-    dislike () {
-        this.dislikesQty +=1
-    }
-}   
+let button1 = document.getElementById ('button1')
+button1.addEventListener ('click', changeColor)
 
-const commentary1 = new Comment ('First comment')
-
-
-let commentary2 = new Comment ('Second comment')
+function changeColor () { 
+    let text1 = document.getElementById ('text1')
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    text1.style.color = '#' + randomColor
+    button1.style.color = '#' + randomColor
+}
 
